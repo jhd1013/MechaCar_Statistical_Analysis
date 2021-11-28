f7d95917c1e4dac644c7114cfa7a3a3286208274
John Davidson <jhd1013@msn.com>
2021-11-28 00:21:48 -0600

    Cleanup R script

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -18,4 +18,11 @@ lot_summary <- coil.psi %>% group_by(Manufacturing_Lot) %>%
 # Population mean is 1500 PSI, determine if Lots 1-3 are statistically
 # different from this mean
 t.test(coil.psi$PSI, mu=1500)
+
+# Check each lot individually
 lot1 <- subset(coil.psi, Manufacturing_Lot == 'Lot1')$PSI
+lot2 <- subset(coil.psi, Manufacturing_Lot == 'Lot2')$PSI
+lot3 <- subset(coil.psi, Manufacturing_Lot == 'Lot3')$PSI
+t.test(lot1, mu=1500)
+t.test(lot2, mu=1500)
+t.test(lot3, mu=1500)
